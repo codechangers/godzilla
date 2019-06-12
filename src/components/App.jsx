59c017e116511a2ad2ec1d922d81fec4a2226fd3
Matt Lant <matthewclant@gmail.com>
2019-06-12 14:59:05 -0600

    redirect to dashboards after login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,22 +10,55 @@ import TrainingTeacher from './Dashboards/TeacherInTraining';
 import Teacher from './Dashboards/TeacherOutOfTraining';
 import Organization from './Dashboards/Organization';
 import '../assets/css/App.css';
+import firebase from '../firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Router>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/parent" component={Parent} />
-        <Route path="/trainingteacher" component={TrainingTeacher} />
-        <Route path="/teacher" component={Teacher} />
-        <Route path="/organization" component={Organization} />
-      </Router>
-    </div>
-  );
-};
+let authSubscription = () => {};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+    this.firebase = firebase();
+  }
+
+  componentDidMount() {
+    console.log(`>>>>>>>${this.firebase}`);
+    authSubscription = this.firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    authSubscription();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Route path="/" exact component={HomePage} />
+
+          {/* <Route path="/login" component={Login} user={this.state.user} firebase={this.firebase} /> */}
+          <Route
+            path="/login"
+            render={props => <Login {...props} user={this.state.user} firebase={this.firebase} />}
+          />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/parent" component={Parent} />
+          <Route path="/trainingteacher" component={TrainingTeacher} />
+          <Route path="/teacher" component={Teacher} />
+          <Route path="/organization" component={Organization} />
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,16 +4,56 @@ import autoBind from '../autoBind';
 import firebase from '../firebase';
 import 'firebase/auth';
 
+const accountTypeToRoute = {
+  '': '/signup',
+  parents: '/parent',
+  organizations: '/organization',
+  teachers: '/trainingteacher'
+};
+
 class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
-      isLoggedIn: false
+      isLoggedIn: false,
+      shouldRedirect: ''
     };
-    this.firebase = firebase();
+    this.firebase = props.firebase;
+    this.db = this.firebase
+      .firestore()
+      .collection('env')
+      .doc('DEVELOPMENT');
     autoBind(this);
+  }
+
+  componentDidMount() {
+    this.getUserDashboard();
+  }
+
+  componentWillReceiveProps(props) {
+    this.getUserDashboard();
+  }
+
+  getUserDashboard() {
+    console.log(this.props.user);
+    if (this.props.user != null) {
+      ['teachers', 'organizations', 'parents'].forEach(collection => {
+        this.db
+          .collection(collection)
+          .doc(this.props.user.uid)
+          .get()
+          .then(doc => {
+            if (doc.exists) {
+              this.setState({
+                shouldRedirect: accountTypeToRoute[collection]
+              });
+              console.log(accountTypeToRoute[collection]);
+            }
+          });
+      });
+    }
   }
 
   handleChange(e) {
@@ -34,7 +74,6 @@ class Login extends React.Component {
       .auth()
       .signInWithEmailAndPassword(email, password)
       .then(() => {
-        console.log('>>>>>>>>>>>logging in');
         this.setState({
           isLoggedIn: true
         });
@@ -51,8 +90,8 @@ class Login extends React.Component {
   }
 
   render() {
-    return this.state.isLoggedIn ? (
-      <Redirect to="/dashboard" />
+    return this.state.shouldRedirect.length > 0 ? (
+      <Redirect to={this.state.shouldRedirect} />
     ) : (
       <div className="login-form">
         <label htmlFor="email">
