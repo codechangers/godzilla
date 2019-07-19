a4a22053c39bb0e63f3c478a594355b32f453136
Matt Lant <matthewclant@gmail.com>
2019-07-19 11:26:36 -0600

    load user in my profile component for parent

--- src/components/Dashboards/ParentDashComponents/Profile.jsx
+++ src/components/Dashboards/ParentDashComponents/Profile.jsx
@@ -7,7 +7,7 @@ class Profile extends React.Component {
     super(props);
     this.state = {
       isLoadingUser: true,
-      currentUser: {}
+      currentUser: null
     };
     this.user = this.props.user;
     this.firebase = this.props.firebase;
@@ -20,25 +20,45 @@ class Profile extends React.Component {
 
   componentDidMount() {
     const newState = {};
-    newState.currentUser = this.db
+    this.db
       .collection('parents')
       .doc(this.user.uid)
       .get()
-      .then(function(doc) {
+      .then(doc => {
         console.log('user: ', doc.data());
-        return doc.data();
+        newState.isLoadingUser = false;
+        newState.currentUser = doc.data();
+        this.setState({ ...newState });
       });
-    this.setState({ ...newState });
-    this.setState({ isLoadingUser: false });
+
+    // newState.currentUser.then(value => {
+    //   this.setState({ ...newState });
+    // });
   }
 
   render() {
-    return this.state.currentUser !== {} ? (
+    return this.state.isLoadingUser === false ? (
       <div>
-        <h1>
-          Testing hello
-          {console.log('state user: ', this.state.currentUser)}
-        </h1>
+        <p>
+          first name:
+          {this.state.currentUser.fName}
+        </p>
+        <br />
+        <p>
+          last name:
+          {this.state.currentUser.lName}
+        </p>
+        <br />
+        <p>
+          email:
+          {this.state.currentUser.email}
+        </p>
+        <br />
+        <p>
+          phone:
+          {this.state.currentUser.phone}
+        </p>
+        {console.log('state user: ', this.state.currentUser.fName)}
       </div>
     ) : (
       <Spinner color="primary" />
