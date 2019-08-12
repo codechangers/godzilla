b7fa78150d8208b09dc53ffce72e6921fbd816cc
Macuyler <macuyler@gmail.com>
2019-08-12 14:22:38 -0500

    Show correct interface for path

---
+++ src/components/Interfaces/Curriculum.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const CurriculumInterface = () => <h1>Curriculum Interface</h1>;
+
+export default CurriculumInterface;
---
+++ src/components/Interfaces/Payment.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const PaymentInterface = () => <h1>Payment Interface</h1>;
+
+export default PaymentInterface;
