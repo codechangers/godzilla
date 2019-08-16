2572a218cd702949f13eb3324a9f39ed813cdc24
Macuyler <macuyler@gmail.com>
2019-08-16 13:20:17 -0500

    Update URL in globals

--- src/globals.js
+++ src/globals.js
@@ -2,7 +2,7 @@ export const phoneValidation = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0
 export const emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
 export const birthDateValidation = /(\d+)(-|\/)(\d+)(?:-|\/)(?:(\d+)\s+(\d+):(\d+)(?::(\d+))?(?:\.(\d+))?)?/; // eslint-disable-line
 
-export const URL = 'http://localhost:3000';
+export const URL = 'https://new-platform-f1070.web.app';
 
 export const months = [
   'January',
