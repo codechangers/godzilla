51de854306193ff29ce3f2decea8f61040d12eb7
Macuyler <macuyler@gmail.com>
2020-01-22 15:18:40 -0600

    Add xs breakpoint to sidebar styles

--- src/components/UI/SideBar.jsx
+++ src/components/UI/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Dashboard,
@@ -8,9 +8,10 @@ import {
   Settings,
   Search,
   Edit,
-  CardGiftcard
+  CardGiftcard,
+  Menu
 } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core';
+import { Fab, makeStyles } from '@material-ui/core';
 import { Link, withRouter } from 'react-router-dom';
 import Logout from './Logout';
 import { Logo } from '../Images';
@@ -28,6 +29,8 @@ const nameToIcon = {
 };
 
 const SideBar = ({ names, baseRoute, location, firebase }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
   const nameToRoute = {
     Dashboard: baseRoute,
     'My Classes': baseRoute,
@@ -50,21 +53,46 @@ const SideBar = ({ names, baseRoute, location, firebase }) => {
   const classes = useStyles();
 
   return (
-    <div className={classes.sidebarWrapper}>
-      <Logo />
-      {names.map(name => {
-        const Icon = nameToIcon[name];
-        return (
-          <Link to={nameToRoute[name]} key={name}>
-            <div className={isSelected(name, location) ? 'selected' : ''} key={name}>
-              <Icon />
-              {name}
-            </div>
-          </Link>
-        );
-      })}
-      <Logout firebase={firebase} className={classes.logoutButton} />
-    </div>
+    <>
+      <Fab
+        onClick={() => setShowMenu(true)}
+        className={classes.menuToggle}
+        aria-label="menu"
+        size="small"
+      >
+        <Menu />
+      </Fab>
+      <div
+        id="offclick"
+        className={classes.sidebarClickoff}
+        style={showMenu ? null : { width: 0 }}
+        onClick={e => {
+          if (e.target.id === 'offclick') {
+            setShowMenu(false);
+          }
+        }}
+      >
+        <div
+          className={
+            showMenu ? `${classes.sidebarWrapper} ${classes.openSidebar}` : classes.sidebarWrapper
+          }
+        >
+          <Logo />
+          {names.map(name => {
+            const Icon = nameToIcon[name];
+            return (
+              <Link to={nameToRoute[name]} key={name} onClick={() => setShowMenu(false)}>
+                <div className={isSelected(name, location) ? 'selected' : ''} key={name}>
+                  <Icon />
+                  {name}
+                </div>
+              </Link>
+            );
+          })}
+          <Logout firebase={firebase} className={classes.logoutButton} />
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -80,33 +108,43 @@ SideBar.defaultProps = {
   baseRoute: '/'
 };
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   sidebarWrapper: {
     position: 'fixed',
     top: 0,
     left: 0,
     width: '96px',
-    height: '100vh',
+    height: '100%',
     boxSizing: 'border-box',
     paddingTop: '40px',
     margin: 0,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'flex-start',
-    alignItems: 'center',
+    alignItems: 'flex-end',
     backgroundColor: 'var(--background-color)',
+    overflow: 'hidden',
+    transition: 'width 300ms ease',
+    zIndex: 10,
+    [theme.breakpoints.down('xs')]: {
+      width: 0
+    },
     '& > *': {
       marginBottom: '20px'
     },
     '& > img': {
-      width: '40px'
+      width: '40px',
+      marginRight: '28px',
+      marginLeft: '28px'
     },
     '& > a': {
       textDecoration: 'none'
     },
     '& > a > div': {
       fontSize: '12px',
-      maxWidth: '80px',
+      width: '80px',
+      marginRight: '8px',
+      marginLeft: '8px',
       display: 'flex',
       flexDirection: 'column',
       alignItems: 'center',
@@ -120,11 +158,43 @@ const useStyles = makeStyles({
     }
   },
   logoutButton: {
+    width: '80px',
     position: 'absolute',
     bottom: '6px',
-    left: 5,
-    right: 5
+    right: '8px',
+    left: '8px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  openSidebar: {
+    [theme.breakpoints.down('xs')]: {
+      width: '96px'
+    }
+  },
+  menuToggle: {
+    display: 'none',
+    [theme.breakpoints.down('xs')]: {
+      display: 'block'
+    },
+    background: 'none',
+    boxShadow: 'none',
+    // position: 'absolute',
+    paddingTop: '7px',
+    top: 5,
+    left: 5
+  },
+  sidebarClickoff: {
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+      height: '100vh',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      zIndex: 9,
+      backgroundColor: 'rgba(0,0,0,0.7)'
+    }
   }
-});
+}));
 
 export default withRouter(SideBar);
