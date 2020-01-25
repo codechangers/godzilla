920619fa810fddbf3c200eede9b01aa49598bd84
Macuyler <macuyler@gmail.com>
2020-01-25 13:22:01 -0600

    Add InfoCardHeader styles back in

--- src/components/Classes/InfoCardHeader.jsx
+++ src/components/Classes/InfoCardHeader.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { LinearProgress } from '@material-ui/core';
+import { LinearProgress, Typography } from '@material-ui/core';
 import { AccessTime, LocationOn } from '@material-ui/icons';
 import { getDateString, getTime } from '../../helpers';
 import { programTypeToText } from '../../globals';
@@ -41,65 +41,43 @@ class InfoCardHeader extends React.Component {
   }
 
   render() {
-    const { cls, children, hideImage } = this.props;
+    const { cls, children, hideImage, classes } = this.props;
     const { teacher, showSchedule } = this.state;
     return (
-      <div className={`infocard-header${children !== null ? ' parent' : ''}`}>
-        <div style={{ width: hideImage ? '100%' : 'auto' }}>
-          <h5>{cls.name}</h5>
+      <div className={`${classes.infoCardHeader} ${children !== null ? classes.parent : ''}`}>
+        <div style={hideImage ? { width: '100%' } : null}>
+          <Typography variant="h5" style={{ marginBottom: '6px' }}>
+            {cls.name}
+          </Typography>
           {children === null || hideImage ? null : (
-            <div className="inliner col">
-              <p>
+            <div className={`${classes.inliner} ${classes.col}`}>
+              <Typography variant="body1">
                 {cls.programType ? (
-                  <span
-                    style={{
-                      backgroundColor: '#8dc63f',
-                      fontSize: '0.8rem',
-                      color: '#fff',
-                      padding: '2px 8px',
-                      marginTop: '-10px',
-                      borderRadius: '4px',
-                      boxSizing: 'border-box'
-                    }}
-                  >
-                    {programTypeToText[cls.programType]}
-                  </span>
+                  <span className={classes.programType}>{programTypeToText[cls.programType]}</span>
                 ) : null}
-              </p>
-              <p>{cls.locationName}</p>
-              <p>
+              </Typography>
+              <Typography variant="body1">{cls.locationName}</Typography>
+              <Typography variant="body1">
                 <LocationOn fontSize="inherit" />
                 {cls.locationAddress}
-              </p>
+              </Typography>
             </div>
           )}
           {children !== null && !hideImage ? null : (
-            <div className="inliner col">
-              <p>
+            <div className={`${classes.inliner} ${classes.col}`}>
+              <Typography variant="body1">
                 {cls.programType ? (
-                  <span
-                    style={{
-                      backgroundColor: '#8dc63f',
-                      fontSize: '0.8rem',
-                      color: '#fff',
-                      padding: '2px 8px',
-                      marginTop: '-10px',
-                      borderRadius: '4px',
-                      boxSizing: 'border-box'
-                    }}
-                  >
-                    {programTypeToText[cls.programType]}
-                  </span>
+                  <span className={classes.programType}>{programTypeToText[cls.programType]}</span>
                 ) : null}
-              </p>
+              </Typography>
             </div>
           )}
-          <div className="inliner">
-            <p>{`Start Date: ${getDateString(cls.startDate)}`}</p>
-            <p>{`End Date: ${getDateString(cls.endDate)}`}</p>
+          <div className={classes.inliner}>
+            <Typography variant="body1">{`Start Date: ${getDateString(cls.startDate)}`}</Typography>
+            <Typography variant="body1">{`End Date: ${getDateString(cls.endDate)}`}</Typography>
           </div>
-          <div className="inliner">
-            <p style={{ textDecoration: 'underline' }}>
+          <div className={classes.inliner}>
+            <Typography variant="body1" style={{ textDecoration: 'underline' }}>
               <button
                 onClick={() => this.setState({ showSchedule: true })}
                 style={{
@@ -113,40 +91,46 @@ class InfoCardHeader extends React.Component {
               >
                 View Schedule
               </button>
-            </p>
-            <p>
+            </Typography>
+            <Typography variant="body1">
               <AccessTime fontSize="inherit" />
               {getTime(cls.startTime)}
-            </p>
-            <p>{`$${cls.price} per Student`}</p>
+            </Typography>
+            <Typography varaint="body1">{`$${cls.price} per Student`}</Typography>
           </div>
           {children !== null && !hideImage ? (
-            <div className="inliner">
-              <p>{`Instructor: ${teacher !== null ? `${teacher.fName} ${teacher.lName}` : ''}`}</p>
-              <p>{`Phone: ${teacher !== null ? teacher.phone : ''}`}</p>
-              <p>{`Email: ${teacher !== null ? teacher.email : ''}`}</p>
+            <div className={classes.inliner}>
+              <Typography variant="body1">{`Instructor: ${
+                teacher !== null ? `${teacher.fName} ${teacher.lName}` : ''
+              }`}</Typography>
+              <Typography variant="body1">{`Phone: ${
+                teacher !== null ? teacher.phone : ''
+              }`}</Typography>
+              <Typography variant="body1">{`Email: ${
+                teacher !== null ? teacher.email : ''
+              }`}</Typography>
             </div>
           ) : (
-            <div className="inliner col">
-              <p>{cls.locationName}</p>
-              <p>
+            <div className={`${classes.inliner} ${classes.col}`}>
+              <Typography variant="body1">{cls.locationName}</Typography>
+              <Typography variant="body1">
                 <LocationOn fontSize="inherit" />
                 {cls.locationAddress}
-              </p>
+              </Typography>
             </div>
           )}
-          <div className="description">{cls.description}</div>
-          <div className="progress-label">
+          <div className={classes.description}>{cls.description}</div>
+          <div className={classes.progressLabel}>
             <h6>Students Sign-up</h6>
             <p>{`${cls.children.length}/${cls.maxStudents} STUDENTS`}</p>
           </div>
           <SignUpsProgress
-            className="progress-bar"
+            className={classes.progressBar}
             variant="determinate"
             color="primary"
             value={(cls.children.length / cls.maxStudents) * 100}
           />
-          <div className="progress-overlay">
+          <div className={classes.progressOverlay}>
             <span style={{ marginLeft: `${(cls.minStudents / cls.maxStudents) * 100}%` }} />
           </div>
           {children || <div className="bottom-line" />}
@@ -166,7 +150,8 @@ InfoCardHeader.propTypes = {
   cls: PropTypes.object.isRequired,
   children: PropTypes.node,
   db: PropTypes.object,
-  hideImage: PropTypes.bool
+  hideImage: PropTypes.bool,
+  classes: PropTypes.object.isRequired
 };
 
 InfoCardHeader.defaultProps = {
@@ -175,4 +160,133 @@ InfoCardHeader.defaultProps = {
   hideImage: false
 };
 
-export default InfoCardHeader;
+const styles = {
+  infoCardHeader: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+    marginBottom: '24px',
+    '& > img': {
+      height: '360px',
+      maxWidth: '58%',
+      maxHeight: '100%',
+      borderRadius: '4px',
+      objectFit: 'cover',
+      backgroundPositon: 'center center'
+    },
+    '& > div': {
+      width: '42%',
+      height: '100%',
+      boxSizing: 'border-box',
+      padding: '24px',
+      paddingBottom: 0
+    }
+  },
+  parent: {
+    '& > img': {
+      height: '474px',
+      maxWidth: '50%',
+      maxHeight: '100%'
+    },
+    '& > div': {
+      width: '50%',
+      '& > button': {
+        width: '100%',
+        marginBottom: '24px'
+      }
+    }
+  },
+  programType: {
+    backgroundColor: '#8dc63f',
+    fontSize: '0.8rem',
+    color: '#fff',
+    padding: '2px 8px',
+    lineHeight: '23px',
+    borderRadius: '4px',
+    boxSizing: 'border-box'
+  },
+  inliner: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: '8px',
+    '& p': {
+      marginRight: '20px',
+      fontSize: '14px',
+      lineHeight: '20px',
+      fontWeight: 500,
+      marginBottom: 0,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      '& > svg': {
+        marginRight: '6px'
+      }
+    },
+    '& p:last-child': {
+      marginRight: 0
+    }
+  },
+  col: {
+    flexDirection: 'column',
+    '& p': {
+      fontSize: '20px',
+      marginBottom: '4px'
+    }
+  },
+  description: {
+    textAlign: 'left',
+    width: '100%',
+    height: '100px',
+    overflow: 'hidden',
+    fontSize: '14px',
+    lineHeight: '20px',
+    marginBottom: '10px'
+  },
+  progressLabel: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    '& h6': {
+      color: 'rgba(0,0,0,0.6)',
+      fontSize: '14px',
+      lineHeight: '24px',
+      fontWeight: 500,
+      margin: 0
+    },
+    '& p': {
+      width: '108px',
+      textAlign: 'left',
+      color: 'rgba(0,0,0,0.6)',
+      fontSize: '10px',
+      lineHeight: '16px',
+      fontWeight: 500,
+      margin: 0,
+      letterSpacing: '1.5px'
+    }
+  },
+  progressBar: {
+    width: '100%',
+    zIndex: 1
+  },
+  progressOverlay: {
+    width: '100%',
+    height: '10px',
+    marginTop: '-8px',
+    marginBottom: '23px',
+    zIndex: 2,
+    position: 'relative',
+    '& > span': {
+      width: '1px',
+      height: '10px',
+      backgroundColor: '#13506e',
+      display: 'block'
+    }
+  }
+};
+
+export default withStyles(styles)(InfoCardHeader);
