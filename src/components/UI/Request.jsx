007e9aa5080cf2966a66601b018ab8cd3f724937
Macuyler <macuyler@gmail.com>
2020-08-01 10:25:59 -0500

    Add status chip to Request

--- src/components/UI/Request.jsx
+++ src/components/UI/Request.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles, Typography, Chip } from '@material-ui/core';
+import { CheckCircle, Block, Info } from '@material-ui/icons';
 
 const propTypes = {
   account: PropTypes.object.isRequired
 };
 
+const ACCEPTED = 'ACCEPTED';
+const DECLINED = 'DECLINED';
+const PENDING = 'PENDING';
+
+const statusColor = {
+  ACCEPTED: 'primary',
+  DECLINED: 'default',
+  PENDING: 'secondary'
+};
+
+const statusIcon = {
+  ACCEPTED: CheckCircle,
+  DECLINED: Block,
+  PENDING: Info
+};
+
+const getStatus = account => {
+  if (account.isVerrified) {
+    return ACCEPTED;
+  } else if (account.isDeclined) {
+    return DECLINED;
+  } else {
+    return PENDING;
+  }
+};
+
 const Request = ({ account }) => {
   const classes = useStyles();
+  const status = getStatus(account);
+  const Icon = statusIcon[status];
   return (
     <div className={classes.wrapper}>
-      <div>
-        <Typography variant="h5">
-          {account.parent.fName} {account.parent.lName}
-        </Typography>
-      </div>
+      <Typography variant="h5">
+        {account.parent.fName} {account.parent.lName}
+      </Typography>
+      <Chip label={status} icon={<Icon />} color={statusColor[status]} />
     </div>
   );
 };
