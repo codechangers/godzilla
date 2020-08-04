08505aff858d979b3131f3f5014522c844e1ee22
Macuyler <macuyler@gmail.com>
2020-08-04 11:09:55 -0500

    Add wrapper and header to StudentIDs

--- src/components/Interfaces/StudentIDs.jsx
+++ src/components/Interfaces/StudentIDs.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { makeStyles, Typography } from '@material-ui/core';
 
 const StudentIDs = () => {
-  return <div>hello</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <Typography variant="h2" className={classes.header}>
+        Student IDs
+      </Typography>
+    </div>
+  );
 };
+
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: '20px 8px 15px 8px'
+  },
+  header: {
+    marginBottom: '15px',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '2rem'
+    }
+  }
+}));
 
 export default StudentIDs;
