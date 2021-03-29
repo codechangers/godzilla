b1e64c9401b7e5748970b2e90178900c5fbf17de
Macuyler <macuyler@gmail.com>
2021-03-29 13:16:40 -0500

    Remove template tutorials

--- src/resources/tutorials.js
+++ src/resources/tutorials.js
@@ -1,34 +1,26 @@
-import analytics from './tutorials/analytics.md';
-import comments from './tutorials/comments.md';
 import downloadfromrepl from './tutorials/downloadfromrepl.md';
 import downloads from './tutorials/downloads.md';
 import images from './tutorials/images.md';
 import introduction from './tutorials/introduction.md';
-import pages from './tutorials/pages.md';
 import pickagame from './tutorials/pickagame.md';
 import setup from './tutorials/setup.md';
 import setup1 from './tutorials/setup1.md';
 import setup2 from './tutorials/setup2.md';
 import splitScreen from './tutorials/splitScreen.md';
 import start from './tutorials/start.md';
-import test from './tutorials/test.md';
 import uploadtoserver from './tutorials/uploadtoserver.md';
 
 const tutorials = {
-  analytics,
-  comments,
   downloadfromrepl,
   downloads,
   images,
   introduction,
-  pages,
   pickagame,
   setup,
   setup1,
   setup2,
   splitScreen,
   start,
-  test,
   uploadtoserver
 };
 
