8f1e363ae8165316edc2e1952d5121d7cc255d4d
Macuyler <macuyler@gmail.com>
2021-03-29 13:34:24 -0500

    Import all tutorials

--- src/resources/tutorials.js
+++ src/resources/tutorials.js
@@ -1,3 +1,4 @@
+// Global
 import downloadfromrepl from './tutorials/downloadfromrepl.md';
 import downloads from './tutorials/downloads.md';
 import images from './tutorials/images.md';
@@ -9,6 +10,140 @@ import setup2 from './tutorials/setup2.md';
 import splitScreen from './tutorials/splitScreen.md';
 import start from './tutorials/start.md';
 import uploadtoserver from './tutorials/uploadtoserver.md';
+// Blocks
+import addAStore from './tutorials/blocks/addAStore.md';
+import addResources from './tutorials/blocks/addResources.md';
+import additionalInputs from './tutorials/blocks/additionalInputs.md';
+import allAboutAnimations from './tutorials/blocks/allAboutAnimations.md';
+import connectionsHelp from './tutorials/blocks/connectionsHelp.md';
+import gameBoundsAndBackground from './tutorials/blocks/gameBoundsAndBackground.md';
+import howToCollide from './tutorials/blocks/howToCollide.md';
+import loginAndHowToPlay from './tutorials/blocks/loginAndHowToPlay.md';
+import newCharacterSet from './tutorials/blocks/newCharacterSet.md';
+import newLocationsSet from './tutorials/blocks/newLocationsSet.md';
+import setupItems from './tutorials/blocks/setupItems.md';
+import setupLeaderboard from './tutorials/blocks/setupLeaderboard.md';
+import useBarrier from './tutorials/blocks/useBarrier.md';
+import useItems from './tutorials/blocks/useItems.md';
+// Horse Game
+import horseGame0 from './tutorials/examples/horseGame.md';
+import horseGame1 from './tutorials/examples/horseGame1.md';
+import horseGame2 from './tutorials/examples/horseGame2.md';
+import horseGame3 from './tutorials/examples/horseGame3.md';
+import horseGame4 from './tutorials/examples/horseGame4.md';
+import horseGame5 from './tutorials/examples/horseGame5.md';
+import horseGame6 from './tutorials/examples/horseGame6.md';
+import horseGame7 from './tutorials/examples/horseGame7.md';
+import horseGame8 from './tutorials/examples/horseGame8.md';
+// Run Game
+import runGame0 from './tutorials/examples/runGame.md';
+import runGame1 from './tutorials/examples/runGame1.md';
+import runGame2 from './tutorials/examples/runGame2.md';
+import runGame3 from './tutorials/examples/runGame3.md';
+import runGame4 from './tutorials/examples/runGame4.md';
+import runGame5 from './tutorials/examples/runGame5.md';
+import runGame6 from './tutorials/examples/runGame6.md';
+import runGame7 from './tutorials/examples/runGame7.md';
+import runGame8 from './tutorials/examples/runGame8.md';
+import runGame9 from './tutorials/examples/runGame9.md';
+// Soccer Game
+import soccerGame0 from './tutorials/examples/soccerGame.md';
+import soccerGame1 from './tutorials/examples/soccerGame1.md';
+import soccerGame2 from './tutorials/examples/soccerGame2.md';
+import soccerGame3 from './tutorials/examples/soccerGame3.md';
+import soccerGame4 from './tutorials/examples/soccerGame4.md';
+import soccerGame5 from './tutorials/examples/soccerGame5.md';
+import soccerGame6 from './tutorials/examples/soccerGame6.md';
+import soccerGame7 from './tutorials/examples/soccerGame7.md';
+import soccerGame8 from './tutorials/examples/soccerGame8.md';
+import soccerGame9 from './tutorials/examples/soccerGame9.md';
+import soccerGame10 from './tutorials/examples/soccerGame10.md';
+// Zombie Game
+import zombieGame0 from './tutorials/examples/zombieGame.md';
+import zombieGame1 from './tutorials/examples/zombieGame1.md';
+import zombieGame2 from './tutorials/examples/zombieGame2.md';
+import zombieGame3 from './tutorials/examples/zombieGame3.md';
+import zombieGame4 from './tutorials/examples/zombieGame4.md';
+import zombieGame5 from './tutorials/examples/zombieGame5.md';
+import zombieGame6 from './tutorials/examples/zombieGame6.md';
+import zombieGame7 from './tutorials/examples/zombieGame7.md';
+import zombieGame8 from './tutorials/examples/zombieGame8.md';
+import zombieGame9 from './tutorials/examples/zombieGame9.md';
+import zombieGame10 from './tutorials/examples/zombieGame10.md';
+import zombieGame11 from './tutorials/examples/zombieGame11.md';
+import zombieGame12 from './tutorials/examples/zombieGame12.md';
+
+const blocks = {
+  addAStore,
+  addResources,
+  additionalInputs,
+  allAboutAnimations,
+  connectionsHelp,
+  gameBoundsAndBackground,
+  howToCollide,
+  loginAndHowToPlay,
+  newCharacterSet,
+  newLocationsSet,
+  setupItems,
+  setupLeaderboard,
+  useBarrier,
+  useItems
+};
+
+const horseGame = {
+  horseGame0,
+  horseGame1,
+  horseGame2,
+  horseGame3,
+  horseGame4,
+  horseGame5,
+  horseGame6,
+  horseGame7,
+  horseGame8
+};
+
+const runGame = {
+  runGame0,
+  runGame1,
+  runGame2,
+  runGame3,
+  runGame4,
+  runGame5,
+  runGame6,
+  runGame7,
+  runGame8,
+  runGame9
+};
+
+const soccerGame = {
+  soccerGame0,
+  soccerGame1,
+  soccerGame2,
+  soccerGame3,
+  soccerGame4,
+  soccerGame5,
+  soccerGame6,
+  soccerGame7,
+  soccerGame8,
+  soccerGame9,
+  soccerGame10
+};
+
+const zombieGame = {
+  zombieGame0,
+  zombieGame1,
+  zombieGame2,
+  zombieGame3,
+  zombieGame4,
+  zombieGame5,
+  zombieGame6,
+  zombieGame7,
+  zombieGame8,
+  zombieGame9,
+  zombieGame10,
+  zombieGame11,
+  zombieGame12
+};
 
 const tutorials = {
   downloadfromrepl,
@@ -21,7 +156,12 @@ const tutorials = {
   setup2,
   splitScreen,
   start,
-  uploadtoserver
+  uploadtoserver,
+  blocks,
+  horseGame,
+  runGame,
+  soccerGame,
+  zombieGame
 };
 
 export default tutorials;
