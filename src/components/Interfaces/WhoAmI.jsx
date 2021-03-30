99be0d0666dd05943210ba64c2f620790c21b8c0
Macuyler <macuyler@gmail.com>
2021-03-30 11:41:18 -0500

    Subscribe to child data in WhoAmI

--- src/components/Interfaces/WhoAmI.jsx
+++ src/components/Interfaces/WhoAmI.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  setWhoAmI: PropTypes.func.isRequired
+  setWhoAmI: PropTypes.func.isRequired,
+  accounts: PropTypes.object.isRequired
 };
 
-const WhoAmInterface = ({ setWhoAmI }) => (
-  <div>
-    <h1>Hello World</h1>
-    <button onClick={() => setWhoAmI('bob')}>Bye!</button>
-  </div>
-);
+const WhoAmInterface = ({ setWhoAmI, accounts }) => {
+  const [children, setChildren] = useState([]);
+
+  /**
+   * Subscribe to the parent's children.
+   */
+  useEffect(
+    () =>
+      accounts.parents.ref.onSnapshot(async parentDoc => {
+        const childrenRefs = parentDoc.data().children || [];
+        const childrenData = [];
+        await Promise.all(
+          childrenRefs.map(async childRef => {
+            const childDoc = await childRef.get();
+            childrenData.push({ ...childDoc.data(), id: childDoc.id, ref: childDoc.ref });
+          })
+        );
+        setChildren(childrenData);
+      }),
+    []
+  );
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      {children.map(child => (
+        <p key={child.id}>{child.fName}</p>
+      ))}
+      <button onClick={() => setWhoAmI('bob')}>Bye!</button>
+    </div>
+  );
+};
 
 WhoAmInterface.propTypes = propTypes;
 
