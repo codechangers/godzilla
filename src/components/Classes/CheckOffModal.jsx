6efca5a6045f5d30e0e4b34a04c3d6632bc0b4d8
Macuyler Dunn <macuyler@gmail.com>
2021-04-01 14:57:10 -0500

    Use a list for CheckOffModal

--- src/components/Classes/CheckOffModal.jsx
+++ src/components/Classes/CheckOffModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { List, ListItem, ListItemText, Button, Typography, makeStyles } from '@material-ui/core';
 import Modal from '../UI/Modal';
 import { useLiveChildren } from '../../hooks/children';
 
@@ -12,6 +12,7 @@ const propTypes = {
 
 const CheckOffModal = ({ open, onClose, childRefs }) => {
   const children = useLiveChildren(childRefs);
+  const classes = useStyles();
   return (
     <Modal
       open={open}
@@ -20,14 +21,37 @@ const CheckOffModal = ({ open, onClose, childRefs }) => {
       description="Check off the progress of participants as they make their way through the competition."
     >
       <Typography variant="h4">Check Off Progress</Typography>
-      {children.map(child => (
-        <Typography variant="body1" key={child.id}>
-          {child.fName}
-        </Typography>
-      ))}
+      <List className={classes.list}>
+        {children.map(child => (
+          <ListItem
+            button
+            key={child.id}
+            className={classes.item}
+            onClick={() => console.log(child.fName)}
+          >
+            <ListItemText primary={`${child.fName} ${child.lName}`} />
+          </ListItem>
+        ))}
+      </List>
+      <Button onClick={onClose} style={{ paddingLeft: 50, paddingRight: 50 }}>
+        Close
+      </Button>
     </Modal>
   );
 };
 CheckOffModal.propTypes = propTypes;
+
+const useStyles = makeStyles({
+  list: {
+    width: '100%',
+    maxWidth: 600,
+    margin: '18px 0',
+    padding: 0,
+    borderBottom: '1px solid #f2f2f2'
+  },
+  item: {
+    borderTop: '1px solid #f2f2f2'
+  }
+});
 
 export default CheckOffModal;
