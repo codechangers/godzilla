0ef1a41f9f3c245d9272677fe9652d1b8f122a62
Macuyler Dunn <macuyler@gmail.com>
2021-04-06 15:04:43 -0500

    Enable dark mode in theme

--- src/components/theme.js
+++ src/components/theme.js
@@ -14,6 +14,7 @@ import { breakpoints } from '../globals';
 export default createMuiTheme({
   // Color Palette
   palette: {
+    type: 'dark',
     primary: {
       light: '#00AFEF',
       main: '#a240b5',
