eb7b65b03584cb7c1b5b381bbe23aec3f5ae02a2
Macuyler Dunn <macuyler@gmail.com>
2021-04-15 12:57:40 -0500

    Add custom games app bar

--- src/components/Interfaces/Games.jsx
+++ src/components/Interfaces/Games.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Button, Typography } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
+import * as Styled from './styles';
 
-const GamesInterface = () => <div>Hello Games</div>;
+const propTypes = {
+  useCustomAppBar: PropTypes.func.isRequired
+};
+
+const GamesInterface = ({ useCustomAppBar }) => {
+  useEffect(() => {
+    useCustomAppBar({
+      title: 'Games',
+      action: (
+        <Button variant="outlined" onClick={() => {}} startIcon={<Add />}>
+          New Game
+        </Button>
+      )
+    });
+  }, []);
+  return (
+    <Styled.PageContent>
+      <Typography variant="h1">Games</Typography>
+    </Styled.PageContent>
+  );
+};
+GamesInterface.propTypes = propTypes;
 
 export default GamesInterface;
