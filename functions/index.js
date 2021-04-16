2cadcd667c3c4c145d471b1186fcd9f23473196e
Macuyler Dunn <macuyler@gmail.com>
2021-04-16 14:32:07 -0500

    Add handleRegistraionPayment cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,10 @@
 const functions = require('firebase-functions');
+const { Stripe } = require('stripe');
 
+const ADMIN_STRIPE_ID = functions.config().stripe.admin_id;
 const CLIENT_SECERET = functions.config().stripe.secret;
+
+const stripe = new Stripe(CLIENT_SECERET);
 
 /**
  * Get the stripe user id of a seller when they connect their stripe account to
@@ -62,3 +66,99 @@ db.collection('stripe').doc(req.body.id).get().then(stripeDoc => {
     }).catch(err => res.json({ error: err, status: 500 }));
 }).catch(err => res.json({ error: err, status: 500 }));
 */
+
+/**
+ * Handle payment for class registration, and save kids to class upon successful transaction.
+ */
+exports.handleRegistraionPayment = functions.firestore
+  .document('/env/{env}/payments/{paymentId}')
+  .onCreate(async (snap) => {
+    const { stripeToken, classRef, seller, parent, promo, kidsToRegister } = snap.data();
+    // TODO: Add null checks for all this stuff...
+    const { stripeID } = await seller.get().data();
+    const { price, name, teacher } = await classRef.get().data();
+    const { fName, lName, email } = await parent.get().data();
+    const [validPromo, promoData] = isPromoValid(promo, teacher.id);
+    const { discountType, discountAmount, uses, limited, startUses } = promoData;
+    try {
+      // Calculate total price with discounts.
+      let total = 0;
+      let numOfDiscounts = 0;
+      const regCount = kidsToRegister.length
+      if (validPromo) {
+        numOfDiscounts = regCount > uses && limited ? uses : regCount;
+        const numOfFullPrice = regCount - numOfDiscounts;
+        total = discountType === '$' ?
+        // ($10 * 10kids) - (5discounts * $5) = $100 - $25 => $75
+          (price * regCount) - (numOfDiscounts * discountAmount) :
+        // ($10 * 5kids) + ($10 * 5discounts * 0.01 * 50) = $50 + $25 => $75 
+          (price * numOfFullPrice) + (price * numOfDiscounts * 0.01 * discountAmount);
+      } else total = price * regCount;
+      total = atLeastZero(total);
+      // Create and run stripe charge.
+      if (total > 0) {
+        const chargeData = {
+          amount: total * 100,
+          currency: "usd",
+          description: `${fName} ${lName} paying for ${regCount} registration(s) for ${name}`,
+          source: stripeToken,
+          receipt_email: email
+        };
+        let charge = null;
+        // Create charge with no fee for market owner.
+        if (stripeID === ADMIN_STRIPE_ID) charge = await stripe.charges.create(chargeData);
+        else {
+          // Create charge with fee for everyone else.
+          charge = await stripe.charges.create({
+              ...chargeData,
+              application_fee_amount: 1000 * regCount
+          }, { stripe_account: stripeID });
+        }
+        await snap.ref.update({ status: charge.status });
+        if (charge.status === 'succeeded') {
+          // Save kids to class.children list.
+          const currentRegisrations = await classRef.get().data().children || [];
+          await classRef.update({ children: [...currentRegisrations, ...kidsToRegister] });
+          // Save class to kids.classes lists.
+          kidsToRegister.forEach(kid => {
+            const currentClasses = await kid.get().data().classes || [];
+            await kid.update({ classes: [...currentClasses, classRef] });
+          });
+          console.log('Successfully Handled Registration Payment!', chargeData.description);
+        } else {
+          console.error('Stripe Payment Failed!', charge.status, charge.failure_code, charge.failure_message);
+        }
+      }
+      // Tick promo code uses.
+      if (numOfDiscounts > 0) {
+        if (limited) await promo.ref.update({ uses: atLeastZero(uses - numOfDiscounts) });
+        else await promo.ref.update({ startUses: startUses + numOfDiscounts });
+      }
+    } catch (error) {
+      await snap.ref.update({ error, status: 'failed' });
+      console.error('Handle Registration Payment Failed!', error);
+    }
+  });
+
+
+/**
+ * Check if a promo code is valid for the given teacher.
+ */
+const isPromoValid = (promo, teacherId) => {
+  if (promo !== null) {
+    const promoDoc = await promo.get();
+    if (
+      promoDoc !== null &&
+      promoDoc.exists &&
+      promoDoc.data().teacher.id === teacherId &&
+      promoDoc.data().active &&
+      !promoDoc.data().deletedOn
+    ) return [true, promoDoc.data()];
+  }
+  return [false, {}];
+};
+
+/**
+ * Round a given number up to zero.
+ */
+const atLeastZero = (num) => num > 0 ? num : 0;
