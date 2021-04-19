a66e18e9a7d18151ef8821b5a17d03659b2aff51
Macuyler Dunn <macuyler@gmail.com>
2021-04-19 18:13:05 -0500

    Add retryStripeSellerAccount function

--- functions/index.js
+++ functions/index.js
@@ -8,43 +8,51 @@ const CLIENT_ID = functions.config().stripe.client_id;
 
 const stripe = new Stripe(CLIENT_SECERET);
 
+exports.createStripeSellerAccount = functions.firestore
+  .document('/env/{env}/stripeSellers/{sellerId}')
+  .onCreate(connectStripeSeller);
+
+exports.retryStripeSellerAccount = functions.firestore
+  .document('/env/{env}/stripeSellers/{sellerId}')
+  .onUpdate((snap, context) => {
+    if (!snap.data().stripeID) connectStripeSeller(snap, context);
+  });
+
 /**
  * Get the stripe user id of a seller when they connect their stripe account to
  * the Code Contest marketplace and store it in the stripeSellers collection.
  */
-exports.createStripeSellerAccount = functions.firestore
-  .document('/env/{env}/stripeSellers/{sellerId}')
-  .onCreate(async (snap, context) => {
-    const { authCode } = snap.data();
+const connectStripeSeller = async (snap, context) => {
+  const { authCode } = snap.data();
 
-    const handleError = async error => {
-      await snap.ref.update({ error: error || 'Create Stripe Account Failed!' });
-      console.error('Create Stripe Seller Account Failed!', error);
-    };
+  const handleError = async error => {
+    await snap.ref.update({ error: error || 'Create Stripe Account Failed!' });
+    console.error('Create Stripe Seller Account Failed!', error);
+  };
 
-    try {
-      // Fetch the stripe_user_id of the new seller.
-      let res = await fetch('https://connect.stripe.com/oauth/token', {
-        method: 'POST',
-        body: JSON.stringify({ code: authCode, grant_type: 'authorization_code' }),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${CLIENT_SECERET}`
-        }
-      });
-      res = await res.json();
-      if (res.error) handleError(res.error);
-      else {
-        // Save the stripe_user_id of the new seller.
-        const { stripe_user_id } = res; // eslint-disable-line
-        const { sellerId } = context.params;
-        await snap.ref.set({ stripeID: stripe_user_id });
-        console.log('Successfully Created Stripe Seller Account!', sellerId);
+  try {
+    // Fetch the stripe_user_id of the new seller.
+    let res = await fetch('https://connect.stripe.com/oauth/token', {
+      method: 'POST',
+      body: JSON.stringify({ code: authCode, grant_type: 'authorization_code' }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${CLIENT_SECERET}`
       }
-    } catch (error) {
-      handleError(error);
+    });
+    res = await res.json();
+    if (res.error) handleError(res.error);
+    else {
+      // Save the stripe_user_id of the new seller.
+      const { stripe_user_id } = res; // eslint-disable-line
+      const { sellerId } = context.params;
+      await snap.ref.set({ stripeID: stripe_user_id });
+      console.log('Successfully Created Stripe Seller Account!', sellerId);
     }
-  });
+  } catch (error) {
+    handleError(error);
+  }
+};
 
 /**
  * Deauthorize the stripe account of a seller when they delete their account.
