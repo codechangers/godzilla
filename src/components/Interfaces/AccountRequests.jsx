86c4998b8bd91835eeefa6dcb820c2544f41027d
Macuyler Dunn <macuyler@gmail.com>
2021-04-23 13:30:31 -0500

    Move account request firestore logic to hooks/accountReqs.js

--- src/components/Interfaces/AccountRequests.jsx
+++ src/components/Interfaces/AccountRequests.jsx
@@ -1,50 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Tabs, Tab, Typography } from '@material-ui/core';
 import RequestList from '../UI/Requests/RequestList';
 import TabPanel from '../UI/TabPanel';
-import { db } from '../../utils/firebase';
+import { useAccountRequests } from '../../hooks/accountReqs';
 
 const AccountRequests = () => {
   const [tabIndex, setTabIndex] = useState(0);
-  const [loadingTeachers, setLoadingTeachers] = useState(false);
-  const [loadingOrgs, setLoadingOrgs] = useState(false);
-  const [teacherReqs, setTeacherReqs] = useState([]);
-  const [orgReqs, setOrgReqs] = useState([]);
-
-  useEffect(() => {
-    const subToReqs = (collection, setReqs, setIsLoading) =>
-      collection.onSnapshot(users => {
-        setIsLoading(true);
-        const requests = [];
-        users.docs.forEach(u => {
-          db.collection('parents')
-            .doc(u.id)
-            .get()
-            .then(parentDoc => {
-              const req = {
-                id: u.id,
-                ref: u.ref,
-                ...u.data(),
-                parent: parentDoc.data()
-              };
-              requests.push(req);
-              if (requests.length === users.docs.length) {
-                requests.sort(
-                  (a, b) => new Date(b.dateApplied.seconds) - new Date(a.dateApplied.seconds)
-                );
-                setReqs(requests);
-                setIsLoading(false);
-              }
-            });
-        });
-      });
-    const cancelTeachers = subToReqs(db.collection('teachers'), setTeacherReqs, setLoadingTeachers);
-    const cancelOrgs = subToReqs(db.collection('organizations'), setOrgReqs, setLoadingOrgs);
-    return () => {
-      cancelTeachers();
-      cancelOrgs();
-    };
-  }, [db]);
+  const teacherReqs = useAccountRequests('teachers');
+  const orgReqs = useAccountRequests('organizations');
 
   const classes = useStyles();
   return (
@@ -57,10 +20,10 @@ const AccountRequests = () => {
         <Tab label="Organization Requests" />
       </Tabs>
       <TabPanel value={tabIndex} index={0} className={classes.panel}>
-        <RequestList reqs={teacherReqs} loading={loadingTeachers} />
+        <RequestList reqs={teacherReqs} />
       </TabPanel>
       <TabPanel value={tabIndex} index={1} className={classes.panel}>
-        <RequestList reqs={orgReqs} loading={loadingOrgs} orgs />
+        <RequestList reqs={orgReqs} orgs />
       </TabPanel>
     </div>
   );
