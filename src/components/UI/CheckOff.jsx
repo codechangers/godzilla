e5e1c541ef04c0196057a5dad621dbc8c1a72cc7
Macuyler Dunn <macuyler@gmail.com>
2021-04-28 14:55:16 -0500

    Add basic checkoff form layout

--- src/components/UI/CheckOff.jsx
+++ src/components/UI/CheckOff.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@material-ui/core';
+import { Button, MenuItem, TextField, makeStyles } from '@material-ui/core';
+import { db, auth } from '../../utils/firebase';
 import { getLiveCheckOffData } from '../../hooks/items';
+import { useUserGames } from '../../hooks/games';
 
 const propTypes = {
   page: PropTypes.string.isRequired
@@ -9,15 +11,69 @@ const propTypes = {
 
 const CheckOff = ({ page }) => {
   const checkOff = getLiveCheckOffData(page);
-  React.useEffect(() => console.log(checkOff), [checkOff]);
+  const games = useUserGames();
+  const [gameId, setGameId] = useState('');
+  const classes = useStyles();
+
+  const createCheckOff = () => {
+    const data = {
+      page,
+      parent: auth.currentUser.uid
+    };
+    db.collection('checkOffs')
+      .doc()
+      .set(data);
+  };
+
   return checkOff !== null ? (
     <div>Checking Off: {page}</div>
   ) : (
-    <Button variant="contained" color="primary" onClick={() => {}}>
-      Check Off
-    </Button>
+    <form
+      className={classes.form}
+      onSubmit={e => {
+        e.preventDefault();
+        createCheckOff();
+      }}
+    >
+      <TextField
+        type="text"
+        label="Game"
+        variant="outlined"
+        value={gameId}
+        onChange={e => setGameId(e.target.value)}
+        className={classes.select}
+        select
+      >
+        {games.map(g => (
+          <MenuItem value={g.id} key={g.id}>
+            {g.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Button variant="contained" color="primary" type="submit">
+        Check Off
+      </Button>
+    </form>
   );
 };
 CheckOff.propTypes = propTypes;
+
+const useStyles = makeStyles({
+  form: {
+    width: '100%',
+    padding: 20,
+    margin: 0,
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexGrow: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  select: {
+    flexGrow: 1,
+    flexShrink: 0,
+    marginRight: 30
+  }
+});
 
 export default CheckOff;
