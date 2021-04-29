6e273cab77ab92c0bce6fe1ab33d817b34212908
Macuyler Dunn <macuyler@gmail.com>
2021-04-29 13:34:39 -0500

    Load game data and render name

--- src/components/Classes/CheckOff/List.jsx
+++ src/components/Classes/CheckOff/List.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { Typography } from '@material-ui/core';
 import { getLiveClassCheckOffsData } from '../../../hooks/items';
+import { useLiveGames } from '../../../hooks/games';
 
 const propTypes = {
   cls: PropTypes.object.isRequired
@@ -9,9 +10,25 @@ const propTypes = {
 
 const CheckOffList = ({ cls }) => {
   const checkOffs = getLiveClassCheckOffsData(cls.id);
-  return checkOffs.map(co => (
+  const gameRefs = useMemo(() => checkOffs.map(co => co.gameRef), [checkOffs]);
+  const games = useLiveGames(gameRefs);
+  const checkOffsWithGameData = useMemo(() => {
+    if (checkOffs.length === games.length) {
+      const gamesMap = {};
+      games.map(g => {
+        gamesMap[g.id] = g;
+        return g;
+      });
+      return checkOffs.map(co => {
+        co.game = gamesMap[co.gameRef.id];
+        return co;
+      });
+    }
+    return [];
+  }, [checkOffs, games]);
+  return checkOffsWithGameData.map(co => (
     <Typography variant="body1" key={co.id}>
-      {co.gameRef.id}
+      {co.game.name}
     </Typography>
   ));
 };
