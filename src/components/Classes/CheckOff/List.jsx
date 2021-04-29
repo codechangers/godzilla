4220ad5a30977dd276e9557960f8918bc7af05d4
Macuyler Dunn <macuyler@gmail.com>
2021-04-29 13:03:34 -0500

    Get teacher checkoffs and render a list of game ids

--- src/components/Classes/CheckOff/List.jsx
+++ src/components/Classes/CheckOff/List.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
+import { getLiveTeacherCheckOffsData } from '../../../hooks/items';
 
 const CheckOffList = () => {
-  return <Typography variant="h4">Hello World</Typography>;
+  const checkOffs = getLiveTeacherCheckOffsData();
+  return checkOffs.map(co => (
+    <Typography variant="body1" key={co.id}>
+      {co.gameRef.id}
+    </Typography>
+  ));
 };
 
 export default CheckOffList;
