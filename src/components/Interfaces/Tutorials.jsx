27e0f01f2c0075836858a612a55d80929b8f03ca
Macuyler Dunn <macuyler@gmail.com>
2021-05-17 11:04:15 -0500

    Set new tutorials home page

--- src/components/Interfaces/Tutorials.jsx
+++ src/components/Interfaces/Tutorials.jsx
@@ -3,12 +3,7 @@ import MarkdownPages from '../Markdown/Pages';
 import tutorials from '../../resources/tutorials';
 
 const TutorialsInterface = props => (
-  <MarkdownPages
-    pages={tutorials}
-    homePage="1) Full Day Overview"
-    whiteList="tutorials"
-    {...props}
-  />
+  <MarkdownPages pages={tutorials} homePage="1) Start 1" whiteList="tutorials" {...props} />
 );
 
 export default TutorialsInterface;
