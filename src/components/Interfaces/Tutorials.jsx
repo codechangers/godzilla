6398e3f4eb5e2c02b66d52fda6b7b14dd82cfb23
Macuyler Dunn <macuyler@gmail.com>
2021-06-01 17:04:20 -0500

    Add preflight refs to tutorials/hello.md

--- src/components/Interfaces/Tutorials.jsx
+++ src/components/Interfaces/Tutorials.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import MarkdownPages from '../Markdown/Pages';
-import tutorials, { PICK_A_GAME } from '../../resources/tutorials';
+import tutorials, { preFlight, PICK_A_GAME } from '../../resources/tutorials';
 
 const TutorialsInterface = props => (
-  <MarkdownPages pages={tutorials} homePage={PICK_A_GAME} whiteList="tutorials" {...props} />
+  <MarkdownPages
+    pages={tutorials}
+    includes={preFlight}
+    homePage={PICK_A_GAME}
+    whiteList="tutorials"
+    {...props}
+  />
 );
 
 export default TutorialsInterface;
--- src/resources/tutorials/hello.md
+++ src/resources/tutorials/hello.md
@@ -1,10 +1,14 @@
 # Welcome To CodeContest
 
+> **Before continuing, make sure you have completed the `Pre Contest` tutorial!**
+
 ### Intro
 
 You are about to get started on building your own custom online multiplayer `.io` **game**!
 
 Before we get started, you need to select what game tutorial you will follow throughout the contest.
+
+[What are my options?](/tutorials/Pick%20a%20Game)
 
 ### Select Game
 
