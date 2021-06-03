78b944182ca346633ef86148b5de4e75700cf39d
Macuyler Dunn <macuyler@gmail.com>
2021-06-03 14:47:28 -0500

    Add SubmitInterface content

--- src/components/Interfaces/Submit.jsx
+++ src/components/Interfaces/Submit.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { Button, Container, Typography, makeStyles } from '@material-ui/core';
 
-const SubmitInterface = () => <h1>Hello World</h1>;
+const SubmitInterface = () => {
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md" className={classes.container}>
+      <Typography variant="h2" align="center" className={classes.heading}>
+        Submit your Game!
+      </Typography>
+      <a href="https://forms.gle/2xHYNyhq2MzjCXh48" target="blank" rel="noreferrer noopener">
+        <Button variant="contained" color="primary" className={classes.button}>
+          Go to form
+        </Button>
+      </a>
+    </Container>
+  );
+};
+
+const useStyles = makeStyles({
+  container: {
+    margin: 'auto auto',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '20px 8px',
+    boxSizing: 'border-box',
+    '& a': {
+      textDecoration: 'none',
+      color: 'auto'
+    }
+  },
+  heading: {
+    marginBottom: 20
+  },
+  button: {
+    padding: '6px 80px'
+  }
+});
 
 export default SubmitInterface;
