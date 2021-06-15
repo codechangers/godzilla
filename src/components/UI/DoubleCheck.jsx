08ecda71000bfc75581b8faaf593c30876a53b68
Macuyler Dunn <macuyler@gmail.com>
2021-06-15 14:35:15 -0500

    Add expand feature to doublecheck modal

--- src/components/UI/DoubleCheck.jsx
+++ src/components/UI/DoubleCheck.jsx
@@ -1,6 +1,8 @@
 import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Typography, makeStyles } from '@material-ui/core';
+import { Button, IconButton, Tooltip, Typography, makeStyles } from '@material-ui/core';
+import { AspectRatio } from '@material-ui/icons';
+import clsx from 'clsx';
 import Modal from './Modal';
 import codeImgs from '../../resources/codeImgs';
 
@@ -18,6 +20,7 @@ const DoubleCheck = ({ page }) => {
   const [show, setShow] = useState(false);
   const [current, setCurrent] = useState(null);
   const [title, setTitle] = useState('');
+  const [zoom, setZoom] = useState(false);
   const [gameSrc, roomSrc] = useMemo(() => toImg(page), [page]);
   const classes = useStyles();
 
@@ -28,15 +31,32 @@ const DoubleCheck = ({ page }) => {
   };
 
   return (
-    <div className={classes.wrapper}>
+    <div>
       <Typography variant="h6">Double Check Your Code!</Typography>
       <Typography variant="body1">
         Here is what our code looked like after completing this step:{' '}
-        <button onClick={() => open(gameSrc, 'game.js')}>game.js</button> -{' '}
-        <button onClick={() => open(roomSrc, 'room.js')}>room.js</button>
+        <Button variant="outlined" onClick={() => open(gameSrc, 'game.js')}>
+          game.js
+        </Button>{' '}
+        -{' '}
+        <Button variant="outlined" onClick={() => open(roomSrc, 'room.js')}>
+          room.js
+        </Button>
       </Typography>
-      <Modal open={show} onClose={() => setShow(false)} className={classes.modal}>
-        <Typography variant="h2">{title}</Typography>
+      <Modal
+        open={show}
+        onClose={() => setShow(false)}
+        className={clsx([classes.modal, { [classes.zoom]: zoom }])}
+      >
+        <div className={classes.row}>
+          <div style={{ display: 'block', width: 48 }} className={classes.iconWrap} />
+          <Typography variant="h2">{title}</Typography>
+          <Tooltip title={zoom ? 'Shrink' : 'Expand'} className={classes.iconWrap}>
+            <IconButton onClick={() => setZoom(!zoom)}>
+              <AspectRatio className={clsx([classes.icon, { [classes.spin]: zoom }])} />
+            </IconButton>
+          </Tooltip>
+        </div>
         <img src={current} alt="Code Example" />
         <Button variant="outlined" onClick={() => setShow(false)} className={classes.closeBtn}>
           Close
@@ -47,22 +67,10 @@ const DoubleCheck = ({ page }) => {
 };
 DoubleCheck.propTypes = propTypes;
 
-const useStyles = makeStyles({
-  wrapper: {
-    '& button': {
-      color: 'inherit',
-      fontSize: 'inherit',
-      cursor: 'pointer',
-      textDecoration: 'underline',
-      background: 'none',
-      border: 'none',
-      padding: 'none',
-      margin: 'none',
-      outline: 'none'
-    }
-  },
+const useStyles = makeStyles(theme => ({
   modal: {
     backgroundColor: '#1D2333',
+    transition: 'all 200ms ease',
     padding: '5px 0',
     '& img': {
       width: '100%'
@@ -71,9 +79,26 @@ const useStyles = makeStyles({
       margin: '12px 0 20px 0'
     }
   },
+  zoom: { maxWidth: '100%', minWidth: '100%' },
+  icon: { transition: 'all 300ms ease' },
+  spin: { transform: 'rotateX(180deg)' },
+  iconWrap: {
+    [theme.breakpoints.down('sm')]: { display: 'none' }
+  },
+  row: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    padding: '0 10px',
+    [theme.breakpoints.down('sm')]: {
+      justifyContent: 'center'
+    }
+  },
   closeBtn: {
     marginBottom: 20,
     padding: '5px 24px'
   }
-});
+}));
 export default DoubleCheck;
