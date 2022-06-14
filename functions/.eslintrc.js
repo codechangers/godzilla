7ed0589772ebab0609d601475d23b4b67da6e9e6
Macuyler Dunn <macuyler@protonmail.com>
2022-06-14 17:34:14 -0500

    Add eslint to functions

---
+++ functions/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    quotes: ['error', 'double']
+  }
+};
