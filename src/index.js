9155a29b8e1ca59f4c1a8cf74494f30b7707fc75
Macuyler Dunn <macuyler@protonmail.com>
2022-06-14 14:00:38 -0500

    Remove serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/index.css';
 import App from './components/App';
-import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
